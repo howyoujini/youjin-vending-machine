d9e65452f9c64772417105da0a9ac76ace291d4d
howyoujini <howyoujini@gmail.com>
2025-01-21 22:09:05 +0900

    feat: set up a project with TypeScript

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <h1>Vending Machine</h1>
+    <p>Hey there! Choose your drink: <b>Cola, Water, or Coffee!</b></p>
+  </div>
+`;
